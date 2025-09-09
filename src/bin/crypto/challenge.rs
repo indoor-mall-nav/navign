c4a876d8ee79999cd01140ded8a760ca319fae6c
Ethan Goh <7086cmd@gmail.com>
2025-09-09 13:34:07 +0800

    chore: lint

--- src/bin/crypto/challenge.rs
+++ src/bin/crypto/challenge.rs
@@ -19,12 +19,12 @@ impl ChallengeManager {
     pub(crate) fn create_challenge_hash(&self, nonce: Nonce, timestamp: u64, server_signature: [u8; 86]) -> [u8; 32] {
         let mut hasher = Sha256::new();
         hasher.update(nonce.as_bytes());
-        hasher.update(&timestamp.to_le_bytes());
-        hasher.update(&server_signature);
+        hasher.update(timestamp.to_le_bytes());
+        hasher.update(server_signature);
         let result = hasher.finalize();
         let mut hash = [0u8; 32];
         hash.copy_from_slice(&result);
         hash
     }
-    
+
 }
