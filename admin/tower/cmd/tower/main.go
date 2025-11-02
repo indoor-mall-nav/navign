a3385141af87acab3ebea634fa16c2e8b67b020d
Ethan Wu <7086cmd@gmail.com>
2025-11-02 18:11:58 +0800

    feat: implement gRPC in go tower [ci skip] (#8)
    * feat: add admin
    
    * feat: grpc go

--- admin/tower/cmd/tower/main.go
+++ admin/tower/cmd/tower/main.go
@@ -7,14 +7,33 @@ import (
 	"os/signal"
 	"syscall"
 
+	socketio "github.com/googollee/go-socket.io"
+	"github.com/googollee/go-socket.io/engineio"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
 	"github.com/indoor-mall-nav/navign/admin/tower/internal/controller"
 )
 
 func main() {
 	entityID := flag.String("entity-id", "", "Entity ID")
 	grpcAddr := flag.String("grpc", "localhost:50051", "Rust orchestrator gRPC address")
-	atcAddr := flag.String("atc", "http://[::1]:8080", "Controller server address")
+	towerAddr := flag.String("tower", "http://[::1]:8080", "Controller server address")
 	flag.Parse()
+
+	creds := grpc.WithTransportCredentials(insecure.NewCredentials())
+
+	grpc_client, err := grpc.NewClient(*grpcAddr, creds)
+
+	if err != nil {
+		log.Fatalf("Failed to create gRPC client: %v", err)
+	}
+
+	defer grpc_client.Close()
+
+	socket := socketio.NewServer(&engineio.Options{})
+
+	defer socket.Close()
 
 	if *entityID == "" {
 		log.Fatal("entity-id is required")
@@ -23,21 +42,21 @@ func main() {
 	startPayload := &controller.Controller{
 		Entity: *entityID,
 		GRPC:   *grpcAddr,
-		Tower:  *atcAddr,
+		Tower:  *towerAddr,
 	}
 
 	// Start controller
 	if err := controller.Start(startPayload); err != nil {
-		log.Fatalf("Failed to start tower: %v", err)
+		log.Fatalf("Failed to start controller: %v", err)
 	}
 
-	log.Printf("Tower running for entity: %s", *entityID)
+	log.Printf("Controller running for entity: %s", *entityID)
 
 	// Graceful shutdown
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
 	<-sigCh
 
-	log.Println("Shutting down tower...")
+	log.Println("Shutting down controller...")
 	controller.Stop()
 }
